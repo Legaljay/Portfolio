06ffd332312c0dc6af97a8fce0d93b53b22d7395
Legaljay <adesinaayobami67@gmail.com>
2023-11-17 14:58:44 +0100

    prog

---
+++ src/components/Interface.jsx
@@ -0,0 +1,320 @@
+import { motion } from "framer-motion";
+import ProjectList from "./ProjectList";
+import { Link } from "react-router-dom";
+import { RiLinkedinFill } from "react-icons/ri";
+import {
+  AiFillTwitterCircle,
+  AiFillGithub,
+  AiOutlineInstagram,
+} from "react-icons/ai";
+
+const Section = (props) => {
+  const { children } = props;
+
+  return (
+    <motion.section
+      className={`
+  h-screen w-screen p-8 max-w-screen-2xl mx-auto
+  flex flex-col items-start justify-center
+  `}
+      initial={{
+        opacity: 0,
+        y: 50,
+      }}
+      whileInView={{
+        opacity: 1,
+        y: 0,
+        transition: {
+          duration: 1,
+          delay: 0.6,
+        },
+      }}
+    >
+      {children}
+    </motion.section>
+  );
+};
+
+export const Interface = () => {
+  return (
+    <div className="flex flex-col items-center w-screen">
+      <AboutSection />
+      <SkillsSection />
+      <Section>
+        <h1 className="text-5xl font-bold text-gray-800 mb-4">Projects</h1>
+        <ProjectList />
+      </Section>
+      <ContactSection />
+    </div>
+  );
+};
+
+const AboutSection = () => {
+  return (
+    <Section>
+      <h1 className="text-6xl font-extrabold leading-snug">
+        Hi, I'm
+        <br />
+        <span className="bg-white px-1 italic">Adesina Ayobami</span>
+      </h1>
+      <motion.p
+        className="text-lg text-gray-600 mt-4"
+        initial={{
+          opacity: 0,
+          y: 25,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 1,
+          delay: 1.5,
+        }}
+      >
+        I am a Frontend developer
+        <br />
+        I code, I build
+      </motion.p>
+      <motion.button
+        className={`bg-indigo-600 text-white py-4 px-8 
+      rounded-lg font-bold text-lg mt-16`}
+        initial={{
+          opacity: 0,
+          y: 25,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 1,
+          delay: 2,
+        }}
+      >
+        Contact me
+      </motion.button>
+    </Section>
+  );
+};
+
+const skills = [
+  {
+    title: "Threejs / React Three Fiber",
+    level: 80,
+  },
+  {
+    title: "React",
+    level: 90,
+  },
+  {
+    title: "Nodejs",
+    level: 70,
+  },
+  {
+    title: "Typescript",
+    level: 50,
+  },
+  {
+    title: "3D Modeling",
+    level: 40,
+  },
+];
+const languages = [
+  {
+    title: "🇫🇷 French",
+    level: 10,
+  },
+  {
+    title: "🇺🇸 English",
+    level: 80,
+  },
+  {
+    title: "yb Yoruba",
+    level: 60,
+  },
+];
+
+const SkillsSection = () => {
+  return (
+    <Section>
+      <motion.div whileInView={"visible"}>
+        <h2 className="text-5xl font-bold">Skills</h2>
+        <div className=" mt-8 space-y-4">
+          {skills.map((skill, index) => (
+            <div className="w-64" key={index}>
+              <motion.h3
+                className="text-xl font-bold text-gray-800"
+                initial={{
+                  opacity: 0,
+                }}
+                variants={{
+                  visible: {
+                    opacity: 1,
+                    transition: {
+                      duration: 1,
+                      delay: 1 + index * 0.2,
+                    },
+                  },
+                }}
+              >
+                {skill.title}
+              </motion.h3>
+              <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
+                <motion.div
+                  className="h-full bg-indigo-500 rounded-full "
+                  style={{ width: `${skill.level}%` }}
+                  initial={{
+                    scaleX: 0,
+                    originX: 0,
+                  }}
+                  variants={{
+                    visible: {
+                      scaleX: 1,
+                      transition: {
+                        duration: 1,
+                        delay: 1 + index * 0.2,
+                      },
+                    },
+                  }}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+        <div>
+          <h2 className="text-5xl font-bold mt-10">Languages</h2>
+          <div className=" mt-8 space-y-4">
+            {languages.map((lng, index) => (
+              <div className="w-64" key={index}>
+                <motion.h3
+                  className="text-xl font-bold text-gray-800"
+                  initial={{
+                    opacity: 0,
+                  }}
+                  variants={{
+                    visible: {
+                      opacity: 1,
+                      transition: {
+                        duration: 1,
+                        delay: 2 + index * 0.2,
+                      },
+                    },
+                  }}
+                >
+                  {lng.title}
+                </motion.h3>
+                <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
+                  <motion.div
+                    className="h-full bg-indigo-500 rounded-full "
+                    style={{ width: `${lng.level}%` }}
+                    initial={{
+                      scaleX: 0,
+                      originX: 0,
+                    }}
+                    variants={{
+                      visible: {
+                        scaleX: 1,
+                        transition: {
+                          duration: 1,
+                          delay: 2 + index * 0.2,
+                        },
+                      },
+                    }}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </motion.div>
+    </Section>
+  );
+};
+
+
+const socialLinks = [
+  {
+    path: "https://twitter.com/a_ayor",
+    icon: <AiFillTwitterCircle className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "https://github.com/Legaljay",
+    icon: <AiFillGithub className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "https://instagram.com/m.o.tunrayo",
+    icon: <AiOutlineInstagram className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "https://www.linkedin.com/ayobami-adesina-56aa6299",
+    icon: <RiLinkedinFill className="group-hover:text-white w-4 h-5" />,
+  },
+];
+
+const ContactSection = () => {
+  const year = new Date().getFullYear();
+  return (
+    <Section>
+      <h2 className="text-5xl font-bold mt-6">Contact me</h2>
+      <div className="mt-8 p-8 rounded-md bg-white w-96 max-w-full">
+        <form onClick={(e) =>  e.preventDefault()}>
+          <label for="name" className="font-medium text-gray-900 block mb-1">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+          />
+          <label
+            for="email"
+            className="font-medium text-gray-900 block mb-1 mt-8"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+          />
+          <label
+            for="email"
+            className="font-medium text-gray-900 block mb-1 mt-8"
+          >
+            Message
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+          />
+          <button className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 " >
+            Submit
+          </button>
+        </form>
+      </div>
+      <footer className="self-center mt-5">
+        <div className="">
+          <div className="flex justify-between flex-col md:flex-row flex-wrap gap-[30px]">
+            <div className="flex flex-col justify-center items-center">
+              <p className="text-[16px] leading-7 font-[400] text-textColor mt-4">
+                Copyright @ {year} developed by Legaljay all right reserved.
+              </p>
+
+              <div className="flex items-center gap-3 mt-4">
+                {socialLinks.map((link) => (
+                  <a href={link.path} key={link.path} className="w-9 h-9 border border-solid border-[#181A1E] rounded-full flex items-center justify-center group hover:bg-primaryColor hover:border-none">
+                    {link.icon}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </Section>
+  );
+};
---
+++ src/components/ProjectList.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import ageCalc from '../assets/ageCalculator.png'
+import Yelp from '../assets/Yelp.png'
+import Product from '../assets/eCOMproduct.png'
+import Card from '../assets/interactiveCard.png'
+import medicare from '../assets/mediCare.png'
+import Raft from '../assets/Raft.png'
+import AfriChow from '../assets/AfriChow.png'
+
+const images = [
+    {
+        img: AfriChow,
+        alt: 'AfriChow',
+        link: 'https://africhow.vercel.app/'
+    },
+    {
+        img: ageCalc,
+        alt: "Age Calculator",
+        link: 'https://legaljay.github.io/AgeCalculator/'
+    },
+    {
+        img: Yelp,
+        alt: "Yelp",
+        link: 'https://legaljay.github.io/Project-CampGround/'
+    },
+    {
+        img: Product,
+        alt: "E-Commerce product page",
+        link: 'https://legaljay.github.io/E-commerce_infoPage/'
+    },
+    {
+        img: Card,
+        alt: "Interactive card",
+        link: 'https://legaljay.github.io/interactive-card-details-form-main/'
+    },
+    {
+        img: medicare,
+        alt: "Medicare website",
+        link: 'https://primer-health.vercel.app/'
+    },
+    {
+        img: Raft,
+        alt: "Rafting Website",
+        link: 'https://raft-alpha67.vercel.app/'
+    },
+]
+
+const ProjectList = () => {
+  return (
+    <div className='flex flex-wrap gap-2 w-[80%] mb-5'>
+        {images.map((image) => (
+            <a key={image.img} href={image.link} className='w-[40%]'><img key={image.img} src={image.img} alt={image.alt} /></a>
+        ))}
+    </div>
+  )
+}
+
+export default ProjectList
